120e7aba927947f6304b8b706cc978801f532022
Nma Shawkat <nmashawket@gmail.com>
2022-12-02 00:29:28 +0300

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+
+function  returnsANamedFunction (){
+    return function namedFunction(){
+        console.log('this is the returned named function');
+    }
+}
+
+function returnsAnAnonymousFunction() { 
+    return () => console.log('ananonymous function returned');
+};
